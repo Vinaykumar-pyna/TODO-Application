e19a857b2e37ff480a2c30409470ece9b7e5aa43
Pyna  Vinay Kumar <pynavk@ipexcel.com>
2024-09-05 11:11:06 +0530

    Initial Commit

---
+++ frontend/src/pages/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div className="container">
+            <h1 className="fade-in">About Me</h1>
+            <p className="fade-in">
+                My name is Vinay Kumar and I am a Software Engineer. I have a passion for MERN Stack Development and love building dynamic and interactive websites. I enjoy working with the latest technologies and exploring new frameworks and libraries. In my free time, I like to play chess and solve puzzles. I find it a great way to exercise my mind and improve my strategic thinking. I am dedicated to continuous learning and staying updated with the latest trends in web development. With my experience and skills, I strive to create robust and user-friendly applications that provide a seamless user experience. I am passionate about delivering high-quality code and solving complex problems.
+            </p>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ frontend/src/pages/ContactPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div className="container">
+            <h1 className="fade-in">Contact Me</h1>
+            <p className="fade-in">Feel free to reach out to me for any inquiries or collaboration opportunities:</p>
+            <ul className="fade-in">
+                <li>Email: s170528@rguktsklm.ac.in</li>
+                <li>Phone: 8309845352</li>
+                <li>LinkedIn: <a href="https://www.linkedin.com/in/vinay-kumar-0887aa219/" className="link">LinkedIn Profile</a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default ContactPage;
